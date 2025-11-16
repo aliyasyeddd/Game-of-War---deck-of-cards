b9708f11b71d8cbe3a5d593da15a7ea27852f547
aliya mahaboob <mahaboobaliya02@gmail.com>
2025-11-16 19:34:35 +0530

    determine winning card

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let deckId
 const cardsContainer = document.getElementById("cards")
 const newDDeckBtn = document.getElementById("new-deck")
 const drawCardBtn = document.getElementById("draw-cards")
+const result = document.getElementById('result')
 
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
@@ -19,7 +20,7 @@ drawCardBtn.addEventListener("click", () => {
     fetch(` https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
-           cardsContainer.children[0].innerHTML =
+            cardsContainer.children[0].innerHTML =
                 `
                   <img src=${data.cards[0].image} class="card" alt="deck of cards">
                 `
@@ -27,5 +28,21 @@ drawCardBtn.addEventListener("click", () => {
                 `
                   <img src=${data.cards[1].image} class="card"  alt="deck of cards">
                 `
+                result.textContent = determineCardWinner(data.cards[0], data.cards[1])
         })
 })
+
+function determineCardWinner(card1, card2) {
+    const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9", 
+    "10", "JACK", "QUEEN", "KING", "ACE"]
+    const card1ValueIndex = valueOptions.indexOf(card1.value)
+    const card2ValueIndex = valueOptions.indexOf(card2.value)
+    
+    if (card1ValueIndex > card2ValueIndex) {
+        return "Card 1 wins!"
+    } else if (card1ValueIndex < card2ValueIndex) {
+        return "Card 2 wins!"
+    } else {
+        return "War!"
+    }
+}
