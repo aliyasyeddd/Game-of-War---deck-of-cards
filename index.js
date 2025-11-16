dd640bdd510f7679851512c0d7ca1b8b7c9a17d9
aliya mahaboob <mahaboobaliya02@gmail.com>
2025-11-16 22:16:42 +0530

    score updating

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 let deckId
+let computerScore = 0
+let myScore = 0
 const cardsContainer = document.getElementById("cards")
 const newDDeckBtn = document.getElementById("new-deck")
 const drawCardBtn = document.getElementById("draw-cards")
 const header = document.getElementById('header')
 const remainingCards = document.getElementById("remaining-cards")
-
+const computerScoreEl = document.getElementById("computer-score")
+const myScoreEl = document.getElementById("my-score")
 
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
@@ -46,8 +49,12 @@ function determineCardWinner(card1, card2) {
     const card2ValueIndex = valueOptions.indexOf(card2.value)
 
     if (card1ValueIndex > card2ValueIndex) {
+        computerScore += 1
+        computerScoreEl.textContent = `Computer score: ${computerScore}`
         return "Computer wins!"
     } else if (card1ValueIndex < card2ValueIndex) {
+        myScore += 1
+        myScoreEl.textContent = `My score: ${myScore}`
         return "You win!"
     } else {
         return "War!"
